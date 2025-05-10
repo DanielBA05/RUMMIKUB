a27ea221202748c703488d75a021322636da16ad
DanielBA05 <luisdanielba3@gmail.com>
2025-05-10 07:54:43 -0600

    Código comentado

--- src/main/java/rummikub/Rummikub.java
+++ src/main/java/rummikub/Rummikub.java
@@ -1,9 +1,10 @@
 package rummikub;
 import javax.swing.SwingUtilities;
 
+// Clase principal que inicia la aplicación Rummikub
 public class Rummikub {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(RegistroJugadores::new); //Inicia programa
+        // Lanza la interfaz de registro de jugadores en el hilo de eventos de Swing
+        SwingUtilities.invokeLater(RegistroJugadores::new); // Inicia programa
     }
 }
-
