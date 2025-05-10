8007d65e042c51ec6a7662c2dfda53d8e0325082
R-C24 <160812642+R-C24@users.noreply.github.com>
2025-05-09 23:33:30 -0600

    Comentarios en combinaciones

--- src/main/java/rummikub/Combinaciones.java
+++ src/main/java/rummikub/Combinaciones.java
@@ -10,33 +10,34 @@ public class Combinaciones {
         setFichas(fichasP);
         setCantFichas(fichasP);
     }
-    
+
     public boolean mismoColor(){
-        int colorBase=fichas.get(0).getColor();
+        int colorBase = fichas.get(0).getColor(); //usamos el primero como color de referencia.
         for (Ficha f : fichas){
-            if (f.getColor()!=colorBase){
-                return f.getColor()==0;
+            if (f.getColor() != colorBase){ //compara este con el color de cada ficha.
+                return f.getColor() == 0; //si el color es 0, entonces todo bien porque es un comodín.
             }
         }
-        return true;
+        return true; //si no encuentra nada raro, retorna true.
     }
     
-    public boolean esCombinacionValida(){
+    public boolean esCombinacionValida(){ //revisa primero si es válido/calza con las características de serie. Sino, revisa las de escalera.
         if (new Serie(fichas).esValido()){
             return true;
         }
         return new Escalera(fichas).esValido();
     }
     
-    public boolean esValido() {
+    public boolean esValido() { //una condición importante es que tengan al menos tres fichas.
         return cantFichas >= 3;
     }
 
+    //Métodos set y get.
     public void setFichas(List<Ficha> fichas) {
         this.fichas = new ArrayList<>(fichas);
     }
 
-    public void setCantFichas(List<Ficha> fichas) {
+    public void setCantFichas(List<Ficha> fichas) { //cantFichas se refiera al tamaño, entonces solo se le saca size a la lista.
         this.cantFichas = fichas.size();
     }
 
