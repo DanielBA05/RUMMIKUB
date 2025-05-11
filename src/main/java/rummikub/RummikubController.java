ac65baaf95a47575abec5e907e9ec8544a343f2d
CheuzEx <114528843+CheuzEx@users.noreply.github.com>
2025-05-10 22:24:46 -0600

    Update RummikubController.java

--- src/main/java/rummikub/RummikubController.java
+++ src/main/java/rummikub/RummikubController.java
@@ -5,19 +5,19 @@ import java.util.List;
 import java.util.Collections;
 
 public class RummikubController {
-    private List<Jugador> jugadores;
-    private int jugadorActualIndex;
-    private Tablero tablero;
-    private MontonFichas monton;
-    private Ficha[][] tableroCopiaInicioTurno;
-    private List<Ficha> fichasAtrilInicioTurno;
-    private Ficha fichaSeleccionada;
-    private boolean turnoTerminado;
-    private boolean fichaRobada;
-    private boolean jugadaValida;
-    private final List<Ficha> fichasColocadasEsteTurno = new ArrayList<>();
+    private List<Jugador> jugadores; //lista con los nombres de los jugadores en la partida
+    private int jugadorActualIndex; //entero para llevar cuenta de a quien le corresponde el turno
+    private Tablero tablero; //objeto de clase tablero para llevar la partida
+    private MontonFichas monton; //objeto monton de fichas de la partida
+    private Ficha[][] tableroCopiaInicioTurno; // una copia para poder restablecer el tablero a como estaba al inicio del turno en caso de que se requiera restablecer(reiniciar tablero)
+    private List<Ficha> fichasAtrilInicioTurno; // lo mismo que el tablero pero ahora aplicado al atril
+    private Ficha fichaSeleccionada; //objeto tipo ficha usada para tener margen de la ficha en actual uso 
+    private boolean turnoTerminado; // booleano para validar turnos
+    private boolean fichaRobada; //booleano para comprobar si el jugador del turno actual ha robado ficha
+    private boolean jugadaValida; // booleano para ir actualizando dependiendo de si la jugada acaba siendo válida o no(empieza en true)
+    private final List<Ficha> fichasColocadasEsteTurno = new ArrayList<>(); // para tener constancia de las fichas del turno actual, esto sirve para evitar errores de lógica 
 
-    public RummikubController(List<String> nombresJugadores) {
+    public RummikubController(List<String> nombresJugadores) {//constructor
         jugadores = new ArrayList<>();
         for (String nombre : nombresJugadores) {
             jugadores.add(new Jugador(nombre));
@@ -32,11 +32,11 @@ public class RummikubController {
         guardarEstadoInicialTurno();
     }
     
-    public List<Jugador> getJugadores() {
+    public List<Jugador> getJugadores() { //método get para pasar el nombre de los jugadores a otras clases
         return Collections.unmodifiableList(jugadores);
     }
     
-    private void repartirFichasIniciales() {
+    private void repartirFichasIniciales() { //método para entregar las 14 fichas iniciales a cada jugador, agarrandolas del tablero
         for (Jugador jugador : jugadores) {
             for (int i = 0; i < 14; i++) {
                 Ficha ficha = monton.robarFicha();
@@ -47,7 +47,7 @@ public class RummikubController {
         }
     }
 
-    private void guardarEstadoInicialTurno() {
+    private void guardarEstadoInicialTurno() { //para  actualizar tablerocopiaInicioTurno
         tableroCopiaInicioTurno = new Ficha[Tablero.FILAS][Tablero.COLUMNAS];
         for (int i = 0; i < Tablero.FILAS; i++) {
             for (int j = 0; j < Tablero.COLUMNAS; j++) {
@@ -63,27 +63,27 @@ public class RummikubController {
         }
     }
 
-    public Jugador getJugadorActual() {
+    public Jugador getJugadorActual() { //método para obtener el jugador del turno actual
         return jugadores.get(jugadorActualIndex);
     }
 
-    public List<Ficha> getFichasJugadorActual() {
+    public List<Ficha> getFichasJugadorActual() { //método para obtener las fichas del jugador que está en su turno
         return getJugadorActual().getFichas();
     }
 
-    public Tablero getTablero() {
+    public Tablero getTablero() { //método get para obtener el tablero
         return tablero;
     }
 
-    public Ficha getFichaSeleccionada() {
+    public Ficha getFichaSeleccionada() { //método get para obtener la ficha que está siendo usada
         return fichaSeleccionada;
     }
 
-    public void setFichaSeleccionada(Ficha ficha) {
+    public void setFichaSeleccionada(Ficha ficha) { //método set para cambiar el valor de fichaseleccionada por un nuevo objeto del tipo ficha
         this.fichaSeleccionada = ficha;
     }
 
-    public void colocarFichaEnTablero(Ficha ficha, int fila, int columna) {
+    public void colocarFichaEnTablero(Ficha ficha, int fila, int columna) { // método para colocar una ficha del tablero mientras se quita del atril del jugador actual
         getJugadorActual().removerFicha(ficha);
         ficha.setX(columna);
         ficha.setY(fila);
@@ -91,7 +91,7 @@ public class RummikubController {
         fichasColocadasEsteTurno.add(ficha); 
     }
 
-    public boolean terminarTurno(List<List<Ficha>> mat) {
+    public boolean terminarTurno(List<List<Ficha>> mat) { //método booleano para actualizar turnoterminado y pasar al siguiente turno, también valida si el jugador perdió fichas(que jugó)
         if (jugadorColocoAlMenosUnaFicha()) {
             if (validarJugada(mat)) {
                 turnoTerminado = true;
@@ -111,7 +111,7 @@ public class RummikubController {
         }
     }
 
-    public void siguienteTurno() {
+    public void siguienteTurno() { //método que resetea todos los métodos y actualiza el index para poder ir correctamente al siguiente turno
         jugadorActualIndex = (jugadorActualIndex + 1) % jugadores.size();
         turnoTerminado = false;
         fichaRobada = false;
@@ -124,16 +124,16 @@ public class RummikubController {
         }
     }
 
-    public void anunciarGanador(Jugador ganador) {
+    public void anunciarGanador(Jugador ganador) { //método que recibe un jugador y retorna ¡Ganador: jugador!
         System.out.println("¡Ganador: " + ganador.getNombre() + "!");
     }
 
-    public boolean validarJugada(List<List<Ficha>> combinaciones) {
+    public boolean validarJugada(List<List<Ficha>> combinaciones) { //método que aprovecha métodos de combinaciones y sus subclases para determinar si la jugada fué válida
         if (combinaciones.isEmpty()){
             return false;
         }
         for (List<Ficha> c : combinaciones){
-            Combinaciones combinacion = new Combinaciones(c);
+            Combinaciones combinacion = new Combinaciones(c); //ciclo for para ir validando las nuevas combinaciones cree el usuario
             if (!combinacion.esCombinacionValida()){
                 return false;
             }
@@ -141,7 +141,7 @@ public class RummikubController {
         return true;
     }
 
-    public void reiniciarJugada() {
+    public void reiniciarJugada() {  //método que aprovecha fichascolocadasesteturno para poder reiniciar correctamente el tablero, también devuelve las fichas al atril del jugador
         getJugadorActual().getFichas().clear();
         fichasColocadasEsteTurno.clear();
         for (int i = 0; i < Tablero.FILAS; i++) {
@@ -171,16 +171,16 @@ public class RummikubController {
                 getJugadorActual().agregarFicha(ficha);
                 fichaRobada = true;
                 jugadaValida = false;
-                siguienteTurno();
+                siguienteTurno(); //roba una ficha y se la quita al montón, si se usa, se pasa de turno automaticamente
             }
         }
     }
     
     public int getMonton() {
-        return monton.getCantidadFichas(); 
+        return monton.getCantidadFichas(); //para obtener el número de fichas del montón
     }
 
-    public boolean validarJugadaInicial() {
+    public boolean validarJugadaInicial() { //para validar la jugada de 30 puntos 
         List<List<Ficha>> combinaciones = getCombinacionesDeFichasColocadas();
         int puntos = 0;
         for (List<Ficha> grupo : combinaciones) {
@@ -194,7 +194,7 @@ public class RummikubController {
     }
 
     
-    public List<List<Ficha>> getCombinacionesDeFichasColocadas() {
+    public List<List<Ficha>> getCombinacionesDeFichasColocadas() { //método para obtener las combinaciones colocadas en el tablero, usando las fichascolocadasesteturno
         List<List<Ficha>> combinaciones = new ArrayList<>();
         boolean[][] visitado = new boolean[Tablero.FILAS][Tablero.COLUMNAS];
         for (Ficha ficha : fichasColocadasEsteTurno) {
@@ -219,17 +219,17 @@ public class RummikubController {
         return combinaciones;
     }
 
-    public boolean jugadorColocoAlMenosUnaFicha() {
+    public boolean jugadorColocoAlMenosUnaFicha() { //método necesario para validar que el jugador hay usado aunque sea una ficha antes de pasar el turno
         int fichasIniciales = fichasAtrilInicioTurno.size();
         int fichasActuales = getJugadorActual().getFichas().size();
         return fichasActuales < fichasIniciales;
     }
     
-    public int getCantidadFichasEnMazo() {
+    public int getCantidadFichasEnMazo() { //método para obtener la cantidad de fichas del monton
         return monton.getCantidadFichas();
     }
     
     public boolean esPrimerTurno() {
-        return !getJugadorActual().haHechoPrimeraJugada();
+        return !getJugadorActual().haHechoPrimeraJugada(); //método para comprobar si el jugador ya hizo su primer turno
     }
 }
